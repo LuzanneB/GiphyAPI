339d6290937062e93aacbe10a876c91ea7ba7990
Luzanne Burt <luzannea@gmail.com>
2019-06-10 16:57:13 -0400

    Header and divs on index, initial array setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GiphyAPI
 GiphyAPI Search
+Having trouble saying no?  Worry not, use a gif!  A compilation of a few common phrases will yield the perfect gif for you to use!
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
     console.log("i'm ready");
+
+// This is an arry with starting buttons
+var no = ["Shaking head no","Not Today","Nope","Maybe","Nah"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
