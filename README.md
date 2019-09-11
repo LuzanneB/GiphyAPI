2e3c8768f4446c9b5554409076a979ebfecd56a9
Luzanne Burt <luzannea@gmail.com>
2019-09-11 11:25:36 -0400

    update remarks and readme

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 GiphyAPI Search
 Having trouble saying no?  Worry not, use a gif!  A compilation of a few common phrases will yield the perfect gif for you to use!
 
+You can: 
+    Select a phrase to view the meme's
+    Enter the phrase you would like to create
+
+    Once the memes appear, click to toggle animate.
+
+
+
+
+## Tech Stack
+HTML5
+CSS
+JavaScript
+AJAX
+JQuery
+Bootstrap
+Giphy API
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,80 +1,80 @@
-$(document).ready(function() {
-    console.log("i'm ready");
+$(document).ready(function () {
+  console.log("i'm ready");
 
-// This is an arry with starting buttons
-var no = ["Shaking head no","Not Today","Nope","Maybe","Nah"];
+  // This is an arry with starting buttons
+  var no = ["Shaking head no", "Not Today", "Nope", "Maybe", "Nah"];
 
 
-function displayGif(){
+  function displayGif() {
     var gifOutput = $(this).attr("data-name");
-    var queryURL ="https://api.giphy.com/v1/gifs/search?q="+gifOutput+"&api_key=0anDkstfLhUnDoq0WlArC7SckFK8E9Ea&limit=10&rating=g";
-// Ajax call
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-  .then(function(response) {
-    // var results = response.data;
-    console.log(response.data);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifOutput + "&api_key=0anDkstfLhUnDoq0WlArC7SckFK8E9Ea&limit=10&rating=g";
+    // Ajax call
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function (response) {
+        // var results = response.data;
+        console.log(response.data);
 
-    for(var i=0;i<10;i++){
+        for (var i = 0; i < 10; i++) {
 
-      var gifDiv = $("<div class = 'meme'>"); 
-      var gifImage = $("<img>");
-      gifImage.addClass("image float-left");
-      gifImage.attr("src", response.data[i].images.fixed_height_still.url);
-      gifImage.attr("data-still",response.data[i].images.fixed_height_still.url);
-      gifImage.attr("data-animate",response.data[i].images.fixed_height.url)
-      gifImage.attr("data-state","still")
-      gifDiv.append(gifImage);
-      $("#memeHolder").prepend(gifDiv);
-    } 
-  });
-}
-
-
+          var gifDiv = $("<div class = 'meme'>");
+          var gifImage = $("<img>");
+          gifImage.addClass("image float-left");
+          gifImage.attr("src", response.data[i].images.fixed_height_still.url);
+          gifImage.attr("data-still", response.data[i].images.fixed_height_still.url);
+          gifImage.attr("data-animate", response.data[i].images.fixed_height.url)
+          gifImage.attr("data-state", "still")
+          gifDiv.append(gifImage);
+          $("#memeHolder").prepend(gifDiv);
+        }
+      });
+  }
 
 
-// this function renders buttons
-function renderButtons(){
+
+
+  // this function renders buttons
+  function renderButtons() {
     $("#gifButtons").empty();
 
-// Loop through phrases
+    // Loop through phrases
 
-    for (var i=0; i< no.length; i++){
-        var b= $("<button>");
-        b.addClass("btn btn-secondary mr-2 mt-2");
-        b.attr("id","gifBtn")
-        b.attr("data-name", no[i]);
-        b.attr("type", "button");
-        b.text(no[i]);
-        $("#gifButtons").append(b);
+    for (var i = 0; i < no.length; i++) {
+      var b = $("<button>");
+      b.addClass("btn btn-secondary mr-2 mt-2");
+      b.attr("id", "gifBtn")
+      b.attr("data-name", no[i]);
+      b.attr("type", "button");
+      b.text(no[i]);
+      $("#gifButtons").append(b);
     }
-}
+  }
 
 
- // This function turns input from form into a button
- $("#phrase").on("click", function(event) {
+  // This function turns input from form into a button
+  $("#phrase").on("click", function (event) {
     event.preventDefault();
     var gif = $("#phrase-input").val().trim();
 
-       no.push(gif);
+    no.push(gif);
 
-    // Calling renderButtons which handles the processing of our movie array
+    // Calling renderButtons which handles the processing of our sayings array
     renderButtons();
   });
 
 
- function changeState(){
-  var state = $(this).attr("data-state");
-  if (state === "still") {
-    $(this).attr("src", $(this).attr("data-animate"));
-    $(this).attr("data-state", "animate");
-  } else {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still");
-  }
-};
+  function changeState() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  };
 
 
 
@@ -83,10 +83,10 @@ function renderButtons(){
 
 
 
-renderButtons();
+  renderButtons();
 
-$(document).on("click", "#gifBtn", displayGif);
-$(document).on("click", ".image", changeState);
+  $(document).on("click", "#gifBtn", displayGif);
+  $(document).on("click", ".image", changeState);
 
 
 
